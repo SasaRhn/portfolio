8dec0af1b41c531712640bb388e2b293743715f7
sabri <sabri.rehouni@epitech.eu>
2023-06-07 14:13:17 +0200

    avancement du portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import { ContactUs } from './components/contact';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Mon Portfolio</h1>
+      </header>
+      <main>
+        <section>
+          <h2>À propos de moi</h2>
+          <p>Je suis passionné par les langages de programmation tels que PHP, HTML et CSS, ainsi que
+par les serveurs et la gestion de bases de données. J'ai également acquis des compétences
+solides en langage SQL au cours de ma première année à Epitech. J'ai notamment travaillé sur
+des projets individuels et en équipe, ce qui m'a permis de développer mes compétences en
+résolution de problèmes et en collaboration.</p>
+        </section>
+        <section>
+          <h2>Projets</h2>
+          <ul>
+            <li>
+              <h3>Puissance 4</h3>
+              <p></p>
+            </li>
+            <li>
+              <h3>My cinema</h3>
+              <p></p>
+            </li>
+            <li>
+              <h3>My</h3>
+              <p></p>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2>Contact</h2>
+          <ContactUs />
+        </section>
+      </main>
+      <footer>
+        <p>© 2023 Mon Portfolio. Tous droits réservés.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
