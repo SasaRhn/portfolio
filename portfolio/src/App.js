cb5c275a64e53ba451a6fcd691895915608e59ae
sabri <sabri.rehouni@epitech.eu>
2023-06-09 11:00:03 +0200

    test

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,37 +1,26 @@
 import React from 'react';
 import './App.css';
 import { ContactUs } from './components/contact';
+import { Slide } from "react-awesome-reveal";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>Mon Portfolio</h1>
+        <Slide delay="5">
+        <h1 className='titre-Aceuil'>Mon Portfolio</h1>
+        </Slide>
       </header>
       <main>
         <section>
-          <h2>À propos de moi</h2>
-          <p>Je suis passionné par les langages de programmation tels que PHP, HTML et CSS, ainsi que
-par les serveurs et la gestion de bases de données. J'ai également acquis des compétences
-solides en langage SQL au cours de ma première année à Epitech. J'ai notamment travaillé sur
-des projets individuels et en équipe, ce qui m'a permis de développer mes compétences en
-résolution de problèmes et en collaboration.</p>
+          <h2>Ma presentation</h2>
         </section>
         <section>
           <h2>Projets</h2>
           <ul>
             <li>
-              <h3>Puissance 4</h3>
+              <h3>My h5ai</h3>
               <img src= "./asset/myh5ai.png"></img>
-              <p></p>
-            </li>
-            <li>
-              <h3>My cinema</h3>
-              <p></p>
-            </li>
-            <li>
-              <h3>My</h3>
-              <p></p>
             </li>
           </ul>
         </section>
