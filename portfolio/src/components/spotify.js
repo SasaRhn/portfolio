d7667d882a8e19576c78ff675da3b0b7a907ea17
sabri <sabri.rehouni@epitech.eu>
2023-06-30 13:44:17 +0200

    nouveau

--- portfolio/src/components/spotify.js
+++ portfolio/src/components/spotify.js
@@ -9,12 +9,7 @@ const Spotify = () => {
     <div className="MyH5ai">
       <Slide triggerOnce={true} duration={2000}>
         <p className="Texte-Projet">
-        Le projet est un jeu de Puissance 4 qui se joue à deux joueurs. 
-        La grille du jeu a des dimensions variables. 
-        Les joueurs placent tour à tour un pion dans la colonne de leur choix, qui coulisse jusqu'à la position la plus basse possible. 
-        Un joueur remporte la partie lorsqu'il aligne au moins 4 de ses pièces horizontalement, verticalement ou en diagonale. La partie se termine lorsqu'un joueur gagne, lorsque la grille est pleine sans possibilité de victoire, ou lorsqu'il n'y a plus de cases disponibles. 
-        Le projet est réalisé sous forme d'un plugin JavaScript modulable. 
-        De plus, il est accompagné d'un environnement Docker pour faciliter le déploiement et la gestion de la complexité technique.
+        Le projet consiste à créer un site web qui exploite une API (Interface de Programmation Applicative). L'API permettra d'échanger des données avec une application externe. Le site utilisera cette API pour afficher et manipuler des informations provenant de l'application. De plus, le projet sera accompagné d'un environnement Docker, ce qui facilitera le déploiement et la gestion de la complexité technique.
         </p>
       </Slide>
       <img className="Image-Projet" src={spotifyimg} alt="spotify" />
