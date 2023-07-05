5acf0abd9171e00522c8254fd932c5150bfcbd13
sabri <sabri.rehouni@epitech.eu>
2023-07-05 14:58:35 +0200

    ajout google

--- portfolio/src/components/puissance4.js
+++ portfolio/src/components/puissance4.js
@@ -9,7 +9,7 @@ const Puissance4 = () => {
     <div className="MyH5ai">
       <Slide triggerOnce={true} duration={2000}>
         <p className="Texte-Projet">
-        Le projet est un jeu de Puissance 4 avec une grille modulable. Les joueurs placent tour à tour un pion dans une colonne, gagnant s'ils alignent 4 pièces. Le jeu se termine en cas de victoire, de grille pleine ou de cases épuisées. Réalisé en tant que plugin JavaScript, personnalisable avec taille de grille et couleurs des joueurs.
+Le projet est un jeu de Puissance 4 avec une grille modulable. Les joueurs placent tour à tour un pion dans une colonne, gagnant s'ils alignent 4 pièces. Le jeu se termine en cas de victoire, de grille pleine ou de cases épuisées. Réalisé en tant que plugin JavaScript, personnalisable avec taille de grille et couleurs des joueurs.
         </p>
       </Slide>
       <img className="Image-Projet" src={puissance4img} alt="puissance4" />
